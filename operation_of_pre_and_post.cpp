2f2b5188a703a6622819176e43c1e5795f082fba
Vishal <vssule@mitaoe.ac.in>
2022-06-17 15:01:57 +0530

    code

---
+++ operation_of_pre_and_post.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a;
+    cout <<"Enter the Number Which you want to perform the operations :"<<endl;
+    cin>>a;
+    a++; 
+    cout<<"this will increment the number :"<<a<<endl;
+    ++a;
+    cout<<"This will increment the number :"<<a<<endl;
+    a=a+1;
+    cout<<"This will increment the number :"<<a<<endl;
+    a--;
+    cout<<"This will decrease the number :"<<a<<endl;
+    --a;
+    cout<<"This will decrease the number :"<<a<<endl;
+    a=a-1;
+    cout<<"This will decrease the number :"<<a<<endl;
+    return 0;
+}
